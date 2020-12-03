444f893e7900c1dc60ce2d93489d3faf02b9cbd4
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-12-03 14:38:10 +0100

    Modif

--- exo2/debug_2.js
+++ exo2/debug_2.js
@@ -1,25 +1,22 @@
 let main = document.getElementById("main");
 
-main.id = "main2";
+main.id = "test";
 
 main.style.display = "hidden";
 
-document.getElementById('main2').innerHTML = "Mon nouveau texte !";
+document.getElementById('test').innerHTML = "Mon nouveau texte !";
 
 let html = main.innerHTML;
 
 alert(html);
 
-if(html) {
+if(html !== main.innerHTML) {
     console.log(html);
 }
 else {
-    let test = document.getElementById('test');
-    if(test) {
-        alert(test);
-    }
+    let test = document.getElementById('test').innerHTML = "Mdr";
+    alert(test);
 }
-
 
 main.style.display = "block";
 
